a641b1f1475b6a484826556f27d34f8ad99b7930
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-01-21 21:56:03 +0100

    Changed function for parsing requirements.txt

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import setuptools
-import pathlib
-import pkg_resources
+from os.path import join, dirname, abspath
+
 
 # Parse requirements.txt for dependencies
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
+def read_requirements(basename):
+    reqs_file = join(dirname(abspath(__file__)), basename)
+    with open(reqs_file) as f:
+        return [req.strip() for req in f.readlines()]
+
+
+required_packages = read_requirements('requirements.txt')
 
 # Parse Readme for long_description
 with open("README.md", "r") as fh:
@@ -25,5 +26,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
-    install_requires=install_requires
+    install_requires=required_packages
 )
