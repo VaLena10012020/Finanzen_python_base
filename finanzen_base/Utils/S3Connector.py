017ea50b50f3f3ac171dc7d30a2604e19e8790d4
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-01-23 18:05:30 +0100

    Setup S3Connector test class with moto

--- finanzen_base/Utils/S3Connector.py
+++ finanzen_base/Utils/S3Connector.py
@@ -11,10 +11,25 @@ class S3Connector:
         self.bucket = self.s3_session.Bucket(bucket_name)
 
     def show_bucket_files(self):
+        files = []
         for s3_file in self.bucket.objects.all():
-            print(s3_file.key)
+            files.append(s3_file.key)
+        return files
 
     def download_file(self, filepath: str, target_path: str):
         file = filepath.split("/")[-1]
         path = filepath.split(file)[0]
         self.s3client.download_file(self.bucket_name, path + file, target_path+file)
+
+    def list_buckets(self):
+        """Returns a list of bucket names."""
+        response = self.s3client.list_buckets()
+        return [bucket["Name"] for bucket in response["Buckets"]]
+
+    def list_objects(self, bucket_name, prefix):
+        """Returns a list all objects with specified prefix."""
+        response = self.s3client.list_objects(
+            Bucket=bucket_name,
+            Prefix=prefix,
+        )
+        return [object["Key"] for object in response["Contents"]]
