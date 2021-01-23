b5c7e4f9f688ff8ac4c81b5d5cddace2687d567d
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-01-23 21:03:50 +0100

    Setup requirements structure for different stages

--- setup.py
+++ setup.py
@@ -3,13 +3,45 @@ from os.path import join, dirname, abspath
 
 
 # Parse requirements.txt for dependencies
-def read_requirements(basename):
-    reqs_file = join(dirname(abspath(__file__)), basename)
-    with open(reqs_file) as f:
+def read_requirement_file(req_file: str):
+    req_files = join(dirname(abspath(__file__)), req_file)
+    with open(req_files) as f:
         return [req.strip() for req in f.readlines()]
 
 
-required_packages = read_requirements('requirements.txt')
+class ReqReader():
+    def __init__(self, basename: str):
+        self.basename = basename
+
+    def read_requirements(self):
+        requirement_txt = read_requirement_file(self.basename)
+        install_token = "-r "
+        # Check for external file dependencies and parse all of them
+        while any(install_token in req for req in requirement_txt):
+            for req in requirement_txt:
+                if install_token in req:
+                    # Get parent path if necessary
+                    file_path = req.split(install_token)[1]
+                    if "/" in file_path:
+                        parent_path = file_path.split("/")[0]
+
+                    # Get packages of external path
+                    new_requirement = read_requirement_file(req.split(install_token)[1])
+
+                    # Check for external file dependencies and if necessary add parent path
+                    for new_req in new_requirement:
+                        if install_token in new_req:
+                            new_requirement.extend([new_req.split(" ")[0]+" "+parent_path+"/"+new_req.split(" ")[1]])
+                            new_requirement.remove(new_req)
+
+                    # Add parsed dependencies
+                    requirement_txt.extend(new_requirement)
+                    requirement_txt.remove(req)
+
+        return requirement_txt
+
+
+required_packages = ReqReader('requirements.txt').read_requirements()
 
 # Parse Readme for long_description
 with open("README.md", "r") as fh:
