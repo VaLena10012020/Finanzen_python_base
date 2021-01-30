964b37e4c9a8d694596f6e2b53c8c3ab8f13f114
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-01-30 21:36:55 +0100

    changed output to dict

--- finanzen_base/Utils/extract_filename.py
+++ finanzen_base/Utils/extract_filename.py
@@ -1,24 +1,24 @@
 from typing import Union
 
 
-def extract_filename(file_path: Union[list, str]) -> list:
+def extract_filename(file_path: Union[list, str]) -> dict:
     """
-    Takes in a file_path or a list of file paths and returns only the file name
-    """
-    def extract_filename_single(path: str):
-        if "/" in path:
-            file_name = path.split("/")[-1]
-        else:
-            file_name = path
-        if file_name == ".":
-            file_name = ""
-        return file_name
+    Takes in a file_path or a list of file paths and returns a dict
 
+    Returns
+    dict(file_path: file_name)
+    """
     if type(file_path) is not list:
         file_path = [file_path]
 
-    file_name = []
+    file_names = {}
     for single_path in file_path:
-        file_name.append(extract_filename_single(single_path))
+        if "/" in single_path:
+            file_name = single_path.split("/")[-1]
+        else:
+            file_name = single_path
+        if file_name == ".":
+            file_name = ""
+        file_names[single_path] = file_name
 
-    return file_name
+    return file_names
