9e45b25fd5732ead38ba26254f7ecfd8d82288a8
Valentin Kuhn <44675313+ValentinKuhn@users.noreply.github.com>
2021-02-06 17:39:13 +0100

    Update finanzen_base/Utils/date_ms.py
    Co-authored-by: Lena <34448387+MagdalenaDeschner@users.noreply.github.com>

--- finanzen_base/Utils/date_ms.py
+++ finanzen_base/Utils/date_ms.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 
 
-def date_to_ms(date_st: str, date_split: str = "-"):
+def date_to_ms(date_st: str, date_split: str = "-") -> return int:
     date_list = date_st.split(date_split)
     date_obj = datetime(int(date_list[0]),
                         int(date_list[1]),
